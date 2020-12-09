c21276f6eae38ad632ebefa12ce65ec6909ab19d
HONGSEONGCHAN <seongchan_@naver.com>
2020-12-09 19:56:56 +0900

    Clear Project

--- react-board/src/App.js
+++ react-board/src/App.js
@@ -3,7 +3,7 @@ import {Component} from 'react';
 import {observer} from 'mobx-react';
 
 class App extends Component {
-  
+
   render() {
       return (
         <div className="app">
@@ -14,7 +14,6 @@ class App extends Component {
       );
     }
   }
-}
 
 // observer = 데이터가 변경될 때마다 View 업데이트
 export default observer(App);
