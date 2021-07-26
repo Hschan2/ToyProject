f31ee23e6840f7eb4bbc38c4427ca36293c35084
HONGSEONGCHAN <seongchan_@naver.com>
2021-07-26 15:37:57 +0900

    Create: Covid-19 Graph Project

---
+++ corona_graph/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const header = () => {
+    return (
+        <header className="header">
+            <h1>COVID-19</h1>
+            <h3>국내</h3>
+        </header>
+    )
+}
+
+export default header
