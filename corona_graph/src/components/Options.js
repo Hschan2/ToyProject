545f9610db001ed8dfbcca8ce79c638e57a36d94
HONGSEONGCHAN <seongchan_@naver.com>
2021-12-14 17:34:00 +0900

    Modify: 일부 나라 추가

--- corona_graph/src/components/Options.js
+++ corona_graph/src/components/Options.js
@@ -4,12 +4,204 @@ const Options = [
         title: "국내"
     },
     {
-        country: "jp",
-        title: "일본"
+        country: "gr",
+        title: "그리스"
+    },
+    {
+        country: "nl",
+        title: "네덜란드"
+    },
+    {
+        country: "tw",
+        title: "대만"
+    },
+    {
+        country: "de",
+        title: "독일"
+    },
+    {
+        country: "ru",
+        title: "러시아"
+    },
+    {
+        country: "my",
+        title: "말레이시아"
+    },
+    {
+        country: "mx",
+        title: "멕시코"
     },
     {
         country: "us",
         title: "미국"
+    },
+    {
+        country: "bd",
+        title: "방글라데시"
+    },
+    {
+        country: "vn",
+        title: "베트남"
+    },
+    {
+        country: "be",
+        title: "벨기에"
+    },
+    {
+        country: "br",
+        title: "브라질"
+    },
+    {
+        country: "sa",
+        title: "사우디아라비아"
+    },
+    {
+        country: "se",
+        title: "스웨덴"
+    },
+    {
+        country: "ch",
+        title: "스위스"
+    },
+    {
+        country: "es",
+        title: "스페인"
+    },
+    {
+        country: "sg",
+        title: "싱가포르"
+    },
+    {
+        country: "ae",
+        title: "아랍에미리트"
+    },
+    {
+        country: "ar",
+        title: "아르헨티나"
+    },
+    {
+        country: "is",
+        title: "아이슬란드"
+    },
+    {
+        country: "ie",
+        title: "아일랜드"
+    },
+    {
+        country: "uk",
+        title: "영국"
+    },
+    {
+        country: "au",
+        title: "오스트레일리아"
+    },
+    {
+        country: "uy",
+        title: "우루과이"
+    },
+    {
+        country: "uz",
+        title: "우즈베키스탄"
+    },
+    {
+        country: "iq",
+        title: "이라크"
+    },
+    {
+        country: "ir",
+        title: "이란"
+    },
+    {
+        country: "eg",
+        title: "이집트"
+    },
+    {
+        country: "it",
+        title: "이탈리아"
+    },
+    {
+        country: "in",
+        title: "인도"
+    },
+    {
+        country: "id",
+        title: "인도네시아"
+    },
+    {
+        country: "jp",
+        title: "일본"
+    },
+    {
+        country: "cn",
+        title: "중국"
+    },
+    {
+        country: "cz",
+        title: "체코"
+    },
+    {
+        country: "cl",
+        title: "칠레"
+    },
+    {
+        country: "qa",
+        title: "카타르"
+    },
+    {
+        country: "ca",
+        title: "캐나다"
+    },
+    {
+        country: "ci",
+        title: "코트디부아르"
+    },
+    {
+        country: "co",
+        title: "콜롬비아"
+    },
+    {
+        country: "hr",
+        title: "크로아티아"
+    },
+    {
+        country: "th",
+        title: "태국"
+    },
+    {
+        country: "tr",
+        title: "터키"
+    },
+    {
+        country: "pk",
+        title: "파키스탄"
+    },
+    {
+        country: "pt",
+        title: "포르투갈"
+    },
+    {
+        country: "pl",
+        title: "폴란드"
+    },
+    {
+        country: "fr",
+        title: "프랑스"
+    },
+    {
+        country: "fi",
+        title: "핀란드"
+    },
+    {
+        country: "ph",
+        title: "필리핀"
+    },
+    {
+        country: "hu",
+        title: "헝가리"
+    },
+    {
+        country: "hk",
+        title: "홍콩"
     },
 ];
 
