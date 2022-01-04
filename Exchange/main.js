47f27981079d735c5fec35b4b68dc5473bbe859a
Hchan2 <39434913+Hschan2@users.noreply.github.com>
2022-01-04 21:15:58 +0900

    페이지 아이콘 추가 & 데이터 입력 값 실시간 업데이트

--- Exchange/main.js
+++ Exchange/main.js
@@ -1,3 +1,4 @@
+// 환율 정보
 let currencyRatio = {
     USD: {
         KRW: 1193.32,
@@ -19,20 +20,32 @@ let currencyRatio = {
     }
 }
 
+// 현재 나라 환율
 let fromCurrency = 'USD';
 
-document.querySelectorAll("#from-currency-list a").forEach((menu) => 
-    menu.addEventListener("click", function() {
+document.querySelectorAll("#from-currency-list a").forEach((menu) =>
+    menu.addEventListener("click", function () {
         document.getElementById("from-button").textContent = this.textContent;
         fromCurrency = this.textContent;
+        convert();
     })
 );
 
+// 바꾸고 싶은 나라의 환율
 let toCurrency = 'USD';
 
-document.querySelectorAll("#to-currency-list a").forEach((menu) => 
-    menu.addEventListener("click", function() {
+document.querySelectorAll("#to-currency-list a").forEach((menu) =>
+    menu.addEventListener("click", function () {
         document.getElementById("to-button").textContent = this.textContent;
         toCurrency = this.textContent;
+        convert();
     })
 );
+
+// 압룍 값 변경 감지 시 실시간 업데이트
+function convert() {
+    let amount = document.getElementById("from-input").value;
+    let convertedAmount = amount * currencyRatio[fromCurrency][toCurrency];
+
+    document.getElementById("to-input").value = convertedAmount;
+}
