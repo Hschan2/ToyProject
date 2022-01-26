e82179f85c06bccfa982c4646f235a07f082e112
Hchan2 <39434913+Hschan2@users.noreply.github.com>
2022-01-26 20:55:46 +0900

    BudgetCard - 예산, 지출 확인 가능한 카드 코드,

--- budget-app/src/component/BudgetCard.js
+++ budget-app/src/component/BudgetCard.js
@@ -4,11 +4,14 @@ import { currencyFormatter } from '../utils'
 import ViewExpenseModal from './ViewExpenseModal'
 
 export default function BudgetCard({ name, amount, max, gray, hideButton, onAddExpenseClick, onViewExpenseClick }) {
+    // 지출 금액에 맞춰 배경색 등 변경을 위한 배열 변수
     const classNames = [
 
     ]
 
+    // 지출 금액이 총 예산 금액보다 크면 배경은 투명도가 10인 빨간색으로 변경
     if (amount > max) classNames.push("bg-danger", "bg-opacity-10")
+    // 위의 조건이 맞지 않으면 기본 배경
     else if (gray) classNames.push("bg-light")
 
     return (
@@ -18,22 +21,26 @@ export default function BudgetCard({ name, amount, max, gray, hideButton, onAddE
                 <Card.Title className="d-flex justify-content-between align-items-baseline fw-normal mb-3">
                     <div className="me-2">{name}</div>
                     <div className="d-flex align-items-baseline">
+                        {/* currencyFormatter - 천 자리 단위 설정 */}
                         {currencyFormatter.format(amount)}
                         {max && (<span className="text-muted fs-6 ms-1">
                             / {currencyFormatter.format(max)}
                         </span>)}
                     </div>
                 </Card.Title>
+                {/* max값이 있으면 ProgressBar 보이기 */}
                 {max && (<ProgressBar className="rounded-pill" variant={getProgressBarVariant(amount, max)} min={0} max={max} now={amount} />)}
+                {/* hideButton이 활성화되어 있지 않으면 아래 버튼들 보이기 */}
                 {!hideButton && (<Stack direction="horizontal" gap="2" className="mt-4">
-                    <Button variant="outline-primary" className="ms-auto" onClick={onAddExpenseClick}>Add Expense</Button>
-                    <Button onClick={onViewExpenseClick} variant="outline-secondary">View Expense</Button>
+                    <Button variant="outline-primary" className="ms-auto" onClick={onAddExpenseClick}>새 지출</Button>
+                    <Button onClick={onViewExpenseClick} variant="outline-secondary">지출 내역</Button>
                 </Stack>)}
             </Card.Body>
         </Card>
     )
 }
 
+// ProgressBar의 값에 따라 일반, 경고, 위험 색상으로 변경
 function getProgressBarVariant(amount, max) {
     const ratio = amount / max
 
