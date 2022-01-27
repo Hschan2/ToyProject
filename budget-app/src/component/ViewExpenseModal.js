e02889d8e51d5f01307ee25e3221d2eba4864c98
Hchan2 <39434913+Hschan2@users.noreply.github.com>
2022-01-27 19:18:13 +0900

    지출 내역 보기

--- budget-app/src/component/ViewExpenseModal.js
+++ budget-app/src/component/ViewExpenseModal.js
@@ -1,10 +1,14 @@
 import { Modal, Button, Stack } from "react-bootstrap";
-import { UNCATEGORIZED_BUDGET_ID, useBudgets } from "../contents/BudgetContext";
+import { UNCATEGORIZED_BUDGET_ID, useBudgets } from "../contexts/BudgetContext";
 import { currencyFormatter } from "../utils";
+import '../App.css'
 
 export default function ViewExpenseModal({ budgetId, handleClose }) {
+    // 지출 내역을 확인하기 위해 지출 내역, 예산, 예산 삭제, 지출 삭제 함수 가져오기
     const { getBudgetExpenses, budgets, deleteBudget, deleteExpense } = useBudgets()
+    // 카테고리 없음일 경우 카테고리 없음의 예산 가져오기
     const budget = UNCATEGORIZED_BUDGET_ID === budgetId ? { name: "카테고리 없음", id: UNCATEGORIZED_BUDGET_ID } : budgets.find(budget => budget.id === budgetId)
+    // ID로 각 지출 내역 가져오기
     const expenses = getBudgetExpenses(budgetId)
 
     return (
@@ -12,8 +16,9 @@ export default function ViewExpenseModal({ budgetId, handleClose }) {
             <Modal.Header closeButton>
                 <Modal.Title>
                     <Stack direction="horizontal" gap="2">
-                        {/* ? 연산자는 undefined or null 처리를 자동 */}
-                        <div>지출 내역 - {budget?.name}</div>
+                        {/* ? 연산자는 undefined or null 자동으로 처리. budget 값이 있으면 name 출력 없으면 undefined or null */}
+                        <div className="sbFont">지출 내역 - {budget?.name}</div>
+                        {/* ID가 카테고리 없음이 아닐 경우 삭제 버튼 활성화 */}
                         {budgetId !== UNCATEGORIZED_BUDGET_ID && (
                             <Button onClick={() => {
                                 deleteBudget(budget)
@@ -29,6 +34,7 @@ export default function ViewExpenseModal({ budgetId, handleClose }) {
             </Modal.Header>
             <Modal.Body>
                 <Stack direction="vertical" gap="3">
+                    {/* 저장된 지출 내역 모두 출력. 설명과 비용 그리고 삭제 버튼 출력 */}
                     {expenses.map(expense => (
                         <Stack direction="horizontal" gap="2" key={expense.id}>
                             <div className="me-auto fs-4">{expense.description}</div>
