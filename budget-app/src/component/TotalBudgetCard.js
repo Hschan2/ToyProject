1e748dc793625db03cfbe1205f5fcc8f596287e4
Hchan2 <39434913+Hschan2@users.noreply.github.com>
2022-01-28 21:38:59 +0900

    총 예산 및 지출 카드, 카테고리 설정하지 않는 예산 카드

--- budget-app/src/component/TotalBudgetCard.js
+++ budget-app/src/component/TotalBudgetCard.js
@@ -1,18 +1,22 @@
-import { useBudgets } from "../contents/BudgetContext"
+import { useBudgets } from "../contexts/BudgetContext"
 import BudgetCard from "./BudgetCard"
 
 export default function TotalBudgetCard() {
+    // 합계를 위한 예산 배열, 지출 배열 가져오기
     const { expenses, budgets } = useBudgets()
+    // 지출 배열에 있는 모든 값 더하기, 없으면 0
     const amount = expenses.reduce(
         (total, expense) => total + expense.amount, 0
     )
+    // 예산 배열에 있는 모든 값 더하기, 없으면 0
     const max = budgets.reduce(
         (total, budget) => total + budget.max, 0
     )
 
+    // 총 예산이 없으면 카드 출력 없음
     if (max === 0) return null
 
     return (
-        <BudgetCard amount={amount} name="Total" gray max={max} hideButton />
+        <BudgetCard amount={amount} name="합계" gray max={max} hideButton />
     )
 }
--- budget-app/src/component/UncategorizedBudgetCard.js
+++ budget-app/src/component/UncategorizedBudgetCard.js
@@ -1,12 +1,15 @@
-import { UNCATEGORIZED_BUDGET_ID, useBudgets } from "../contents/BudgetContext"
+import { UNCATEGORIZED_BUDGET_ID, useBudgets } from "../contexts/BudgetContext"
 import BudgetCard from "./BudgetCard"
 
 export default function UncategorizedBudgetCard(props) {
+    // 지출 내역 가져오기
     const { getBudgetExpenses } = useBudgets()
+    // 카테고리 없음이라는 ID를 가진 지출 총액을 가져오기, 없으면 0
     const amount = getBudgetExpenses(UNCATEGORIZED_BUDGET_ID).reduce(
         (total, expense) => total + expense.amount, 0
     )
 
+    // 총액이 없으면 카테고리 없음 카드 출력 없음
     if (amount === 0) return null
 
     return (
