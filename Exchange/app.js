8068cb6c6555e42f8060c686af377bb02309c66f
Hchan2 <39434913+Hschan2@users.noreply.github.com>
2022-04-19 20:10:38 +0900

    Library 설지

--- Exchange/app.js
+++ Exchange/app.js
@@ -1,42 +1,22 @@
-// const http = require('http');
 const express = require('express');
-const { request } = require('http');
-const app = express();
-// const server = createServer(app);
-// const cors = require('cors');
 const bodyParser = require('body-parser');
+const request = require('request');
+const app = express();
+const ejs = require('ejs');
 
 const PORT = 3000;
 const apiUrl = 'https://www.koreaexim.go.kr/site/program/financial/exchangeJSON?authkey=lqnoRB6bPy49PWbH5KuV2dZwLfdSLT2s&data=AP01';
 
-// 모든 도메인에 CORS 허용
-// app.use(cors());
-// 특정 도메인에 CORS 허용
-// app.use(cors(corsOptions));
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html')
 
-// for CSS, Javascript files
-// app.use(express.static(__dirname + "/"));
+// API 호출 결과값을 Route 결과로 전달 (json 형식으로 전달)
+// request가 아닌 app의 res로 보내야 한다. (request에서 json 형식은 없다.)
 
-// index.html을 기본페이지로
-// app.get("/", (req, res) => {
-//     res.sendFile(__dirname + "/index.html");
-// });
-
-// 404에러 발생 시 사용할 페이지
-// app.use((req, res) => {
-//     res.sendFile(__dirname + "/404.html");
-// })
-
-app.use(bodyParser.json());
-
-app.get('/', (req, res) => {
-    request(apiUrl, (err, res, body) => {
-        if (body == null) console.log(err);
-
-        // API 호출 결과값을 Route 결과로 전달 (json 형식으로 전달)
-        res.json(body);
-        console.log(body);
-    })
+app.get("/", (req, res) => {
+    res.render("index.html");
 })
 
 app.listen(PORT, (err) => {
