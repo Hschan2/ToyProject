89de91980392f06d7c00a8aad81ab638718ed80f
Hchan2 <seongchan_@naver.com>
2022-09-05 13:40:06 +0900

    LottieFiles 컴포넌트화

---
+++ movie/frontend/src/components/lottieFiles.js
@@ -0,0 +1,23 @@
+import React, { useRef } from 'react'
+import { Player } from '@lottiefiles/react-lottie-player';
+
+/**
+ * Lottiefiles 애니메이션 컴포넌트
+ * Lottiefiles 출처: https://lottiefiles.com/
+ * @returns 
+ */
+function LottieFiles({ srcLink, style }) {
+    const container = useRef(null);
+
+    return (
+        <Player
+            ref={container.current}
+            autoplay
+            loop
+            src={srcLink}
+            className={style}
+        />
+    )
+}
+
+export default LottieFiles
