f59e8d4c1521af573e415961613a7290c71bdee5
Hchan2 <seongchan_@naver.com>
2022-09-05 13:40:44 +0900

    분리된 LottieFiles 컴포넌트 활용

--- movie/frontend/src/components/Loading.js
+++ movie/frontend/src/components/Loading.js
@@ -1,25 +1,17 @@
-import React, { useRef } from 'react'
+import React from 'react'
 import styles from '../style/loading.module.css';
-import { Player } from '@lottiefiles/react-lottie-player';
+import LottieFiles from './lottieFiles';
 
 /**
  * 로딩 페이지 컴포넌트
- * Lottiefiles 출처: https://lottiefiles.com/
  * @returns 로딩 페이지 출력
  */
 function Loading() {
-  const container = useRef(null);
+  const lottieFilesSrc = 'https://assets1.lottiefiles.com/packages/lf20_jv0xz0qi.json';
 
   return (
     <div className={styles.container}>
-      <div className={styles.loading}></div>
-      <Player
-        ref={container.current}
-        autoplay
-        loop
-        src='https://assets1.lottiefiles.com/packages/lf20_jv0xz0qi.json'
-        className={styles.loading}
-      />
+      <LottieFiles srcLink={lottieFilesSrc} style={styles.loading} />
       <div className={styles.message}>Loading...</div>
     </div>
   )
