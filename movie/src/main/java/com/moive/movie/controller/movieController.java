5a0e05e5e37522c2e72996399db01dd133ce40bb
Hchan2 <seongchan_@naver.com>
2022-11-01 20:26:16 +0900

    오타 수정

--- movie/src/main/java/com/moive/movie/controller/movieController.java
+++ movie/src/main/java/com/moive/movie/controller/movieController.java
@@ -12,43 +12,65 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+/**
+ * 영화 목록 API 데이터 받는 Controller 설명
+ *
+ * @author Hong Seong Chan
+ * 작성일 2022-09-25
+ * */
+
 @RestController
 public class movieController {
-
+    
+//    API-KEY.properties의 API key 가져오기
     @Value("${api-key}")
     private String API_KEY;
 
+//    인기 영화 목록 가져오기
     @GetMapping("/api/popular")
     public String popular() throws IOException {
+//        인기 영화 카테고리 변수 선언
         String category = "popular";
 
+//        인기 영화 카테고리 변수를 API를 파싱하는 함수에 전달
         return apiParse(category);
     }
 
+//    평점순 영화 목록 가져오기
     @GetMapping("/api/highRated")
     public String highRated() throws IOException {
+//        평점순 카테고리 변수 선언
         String category = "top_rated";
 
+//        평점순 카테고리 변수를 API 파싱하는 함수에 전달
         return apiParse(category);
     }
 
+//    개봉예정 영화 목록 가져오기
     @GetMapping("/api/upcoming")
     public String upcoming() throws IOException {
+//        개봉예정 카테고리 변수 선언
         String category = "upcoming";
 
+//        개봉예정 카테고리 변수를 API 파싱하는 함수에 전달
         return apiParse(category);
     }
 
+//    영화 ID값을 받아 상세페이지 이동
     @GetMapping("/api/detail/{id}")
     public String detail(@PathVariable("id") String id) throws IOException {
-        String category = id;
-
-        return apiParse(category);
+//        Path로 받은 ID 값을 변수에 담기
+        String movieId = id;
+        
+//        특정 영화 ID를 API 파싱하는 함수에 전달
+        return apiParse(movieId);
     }
 
+//    전달받은 파라미터로 API 데이터 호출하기
     public String apiParse(String Category) throws IOException {
         StringBuilder result = new StringBuilder();
 
+//        특정 영화 데이터 목록을 가져올 실제 API 주소
         String urlStr = "https://api.themoviedb.org/3/movie/" + Category + "?api_key=" + API_KEY + "&language=ko-KR";
 
         URL url = new URL(urlStr);
