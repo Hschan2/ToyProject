f6cc6cbd61736ac438eb761832e5452354ca6a75
Hchan2 <seongchan_@naver.com>
2023-01-13 15:31:30 +0900

    Gateway 504 에러 해결

---
+++ movie/frontend/src/components/GetSearchResult.js
@@ -0,0 +1,25 @@
+import axios from "axios"
+
+/**
+ * 영화 검색 결과 데이터 Get 컴포넌트
+ * @param apiUrl API 주소 파라미터
+ * @param setData API에서 가져온 데이터를 담을 파라미터
+ * @param Text 검색을 위한 입력값
+ * @returns 
+ */
+function GetSearchResult(apiUrl, setData, searchText) {
+
+  return async () => {
+    try {
+      const getData = await axios.get(apiUrl, {
+        params: { searchText: searchText },
+      });
+      setData(getData.data.results);
+    }
+    catch(e) {
+      console.log(`${setData} error: ` , e);
+    }
+  }
+}
+
+export default GetSearchResult
--- movie/frontend/src/pages/Popular.js
+++ movie/frontend/src/pages/Popular.js
@@ -5,24 +5,22 @@ import MovieLists from '../components/MovieLists';
 import Footer from '../components/Footer';
 import Loading from '../components/Loading';
 import GetMovieList from '../components/GetMovieList';
+import GetSearchResult from '../components/GetSearchResult';
 
 /**
  * 인기 영화 목록 출력 컴포넌트
  * @popularData 인기 영화 목록 데이터 담을 변수
  */
-function Popular({ searchData }) {
+function Popular({ searchData, setSearchData }) {
   const [popularData, setPopularData] = useState();
-  const [searchResultData, setSearchResultData] = useState();
-
+  // const [searchResultData, setSearchResultData] = useState();
+  const getPopularData = GetMovieList('/api/popular', setPopularData);
+// const getSearchResultData = GetSearchResult('/api/search', setSearchResultData, searchData);
+  
   useEffect(() => {
     getPopularData();
-    getSearchResultData();
-  }, []);
-
-  console.log(searchResultData);
-
-  const getPopularData = GetMovieList('/api/popular', setPopularData);
-  const getSearchResultData = GetMovieList(`/api/search/${searchData}`, setSearchResultData);
+    // getSearchResultData();
+  }, [getPopularData]);
 
   return (
     <div>
