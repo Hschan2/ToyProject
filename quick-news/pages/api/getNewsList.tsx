2348db3a79366d0c09d75ab2f37105fa11f9a9f1
Hchan2 <seongchan_@naver.com>
2023-02-08 16:39:33 +0900

    필요 라이브러리 설치, 네이버 뉴스 API 연동 및 호출 함수 생성

---
+++ quick-news/pages/api/getNewsList.tsx
@@ -0,0 +1,39 @@
+import axios from 'axios'
+
+const CLIENT_ID = process.env.ClientID;
+const CLIENT_SECRET = process.env.ClientSecret;
+const BASE_PATH = "/v1/search/news.json?";
+
+interface News {
+    title: string;
+    originallink: string;
+    link: string;
+    description: string;
+    pubData: string;
+}
+
+export interface getNewsListResult {
+    items: News[];
+}
+
+export async function NewsList() {
+    return axios.get(`${BASE_PATH}`, {
+        params: {
+            query: "사회",
+            sort: "sim",
+            display: 4,
+        },
+        headers: {
+            "X-Requested-With": "XMLHttpRequest",
+            "X-Naver-Client-Id": CLIENT_ID,
+            "X-Naver-Client-Secret": CLIENT_SECRET,
+        },
+    })
+        .then(res => res.data)
+        .catch(err => {
+            console.log(`에러발생: ${err}`);
+            if (err.status === 403) {
+                console.log(`404 에러 페이지로 이동`);
+            }
+        })
+}
