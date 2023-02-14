47b3bde6bb479656c20846f2c807ff00c63184fe
Hchan2 <seongchan_@naver.com>
2023-02-14 21:13:41 +0900

    음악 목록 정리

--- music-player/contants/music_list.js
+++ music-player/contants/music_list.js
@@ -1,38 +1,38 @@
 export let allMusic = [
     {
-        name: "Harley Bird - Home",
-        artist: "Jordan Schor",
-        img: "../files/images/music-1.jpg",
-        src: "../files/songs/music-1.mp3"
+      name: "Harley Bird - Home",
+      artist: "Jordan Schor",
+      img: "music-1",
+      src: "music-1"
     },
     {
-        name: "Ikson Anywhere – Ikson",
-        artist: "Audio Library",
-        img: "../files/images/music-2.jpg",
-        src: "../files/songs/music-2.mp3"
+      name: "Ikson Anywhere – Ikson",
+      artist: "Audio Library",
+      img: "music-2",
+      src: "music-2"
     },
     {
-        name: "Beauz & Jvna - Crazy",
-        artist: "Beauz & Jvna",
-        img: "../files/images/music-3.jpg",
-        src: "../files/songs/music-3.mp3"
+      name: "Beauz & Jvna - Crazy",
+      artist: "Beauz & Jvna",
+      img: "music-3",
+      src: "music-3"
     },
     {
-        name: "Hardwind - Want Me",
-        artist: "Mike Archangelo",
-        img: "../files/images/music-4.jpg",
-        src: "../files/songs/music-4.mp3"
+      name: "Hardwind - Want Me",
+      artist: "Mike Archangelo",
+      img: "music-4",
+      src: "music-4"
     },
     {
-        name: "Jim - Sun Goes Down",
-        artist: "Jim Yosef x Roy",
-        img: "../files/images/music-5.jpg",
-        src: "../files/songs/music-5.mp3"
+      name: "Jim - Sun Goes Down",
+      artist: "Jim Yosef x Roy",
+      img: "music-5",
+      src: "music-5"
     },
     {
-        name: "Lost Sky - Vision NCS",
-        artist: "NCS Release",
-        img: "../files/images/music-6.jpg",
-        src: "../files/songs/music-6.mp3"
+      name: "Lost Sky - Vision NCS",
+      artist: "NCS Release",
+      img: "music-6",
+      src: "music-6"
     },
-]
+];
