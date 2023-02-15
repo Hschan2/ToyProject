f519d36e91e2e67db81b0b38d8130a7655cc17d6
Hchan2 <seongchan_@naver.com>
2023-02-15 19:17:04 +0900

    데이터 불러오기 에러

--- quick-news/pages/index.tsx
+++ quick-news/pages/index.tsx
@@ -5,18 +5,18 @@ import Seo from './components/Seo'
 
 export default function Home() {
   // const { data, isLoading, error } = useQuery<getNewsListResult>(["news"], NewsList);
-  const { data: nData, isLoading: isNLoading, error: nError } = useQuery<getNewsListResult>(
-    ["news"],
-    NewsList
-  );
+  // const { data: nData, isLoading: isNLoading, error: nError } = useQuery<getNewsListResult>(
+  //   ["news"],
+  //   NewsList
+  // );
 
-  if (isNLoading) return 'Loading...'
-  if (nError) return '에러 발생: ' + nError
+  // if (isNLoading) return 'Loading...'
+  // if (nError) return '에러 발생: ' + nError
 
   return (
     <div>
       <Seo title="사회" />
-      <NewsLists data={nData} />
+      {/* <NewsLists data={nData} /> */}
     </div>
   )
 }
