a4e911e61ad45edda8e1d472676913beab9d2058
Hchan2 <seongchan_@naver.com>
2023-05-26 22:06:52 +0900

    Loading 생성

---
+++ quick-news/pages/components/page/Loading.tsx
@@ -0,0 +1,37 @@
+import {
+  LoadingContainer,
+  LoadingLottie,
+  LoadingMessage,
+} from '@/styles/styledComponents'
+import React, { useState, useEffect } from 'react'
+
+export default function Loading() {
+  const lottieFilesSrc =
+    'https://assets1.lottiefiles.com/packages/lf20_jv0xz0qi.json'
+  const [animationData, setAnimationData] = useState(null)
+
+  useEffect(() => {
+    const fetchAnimationData = async () => {
+      try {
+        const response = await fetch(lottieFilesSrc)
+        const jsonData = await response.json()
+        setAnimationData(jsonData)
+      } catch (error) {
+        console.error('Failed to fetch animation data:', error)
+      }
+    }
+
+    fetchAnimationData()
+  }, [])
+
+  if (!animationData) {
+    return <div>LottieFile Error</div>
+  }
+
+  return (
+    <LoadingContainer>
+      <LoadingLottie loop animationData={animationData} play />;
+      <LoadingMessage>Wait...</LoadingMessage>
+    </LoadingContainer>
+  )
+}
