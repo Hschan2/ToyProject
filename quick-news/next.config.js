875032988f4c9e44b2e1804d489b5aa3521e0b05
Hchan2 <seongchan_@naver.com>
2023-06-02 22:18:55 +0900

    페이지 확장, pageExtensions 추가

--- quick-news/next.config.js
+++ quick-news/next.config.js
@@ -1,18 +1,18 @@
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
   compiler: {
     styledComponents: true,
   },
+  pageExtensions: ['mdx', 'md', 'jsx', 'js', 'tsx', 'ts'],
   async rewrites() {
     return [
       {
-        source: "/:path*",
-        destination: "https://openapi.naver.com/:path*",
-      }
+        source: '/:path*',
+        destination: 'https://openapi.naver.com/:path*',
+      },
     ]
-  }
+  },
 }
 
 module.exports = nextConfig
