e19ce21904fee442b3f9a36bc4175da483ad5884
Hchan2 <seongchan_@naver.com>
2023-06-02 21:48:01 +0900

    eslint 형식에 맞게 수정

--- quick-news/pages/constants/variable.ts
+++ quick-news/pages/constants/variable.ts
@@ -1,4 +1,6 @@
 import { useRouter } from 'next/router'
 
-const ROUTER = useRouter()
-export const CURRENT_URL = ROUTER.asPath
+export const useCurrentUrl = () => {
+  const router = useRouter()
+  return router.asPath
+}
