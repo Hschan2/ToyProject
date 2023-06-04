46e193076ba32988b98d6f7b7465aefd70b3fa31
Hchan2 <seongchan_@naver.com>
2023-06-04 20:25:39 +0900

    네이버 프록시, try/catch 문 적용

--- quick-news/pages/api/naver-news-proxy.ts
+++ quick-news/pages/api/naver-news-proxy.ts
@@ -7,20 +7,25 @@ export default async function handler(
 ) {
   const { query } = req
   res.setHeader('Access-Control-Allow-Origin', '*')
-  const apiResponse = await axios.get(
-    `https://openapi.naver.com/v1/search/news.json`,
-    {
-      headers: {
-        'X-Naver-Client-Id': process.env.NEXT_PUBLIC_CLIENT_ID,
-        'X-Naver-Client-Secret': process.env.NEXT_PUBLIC_CLIENT_SECRET,
+  try {
+    const apiResponse = await axios.get(
+      `https://openapi.naver.com/v1/search/news.json`,
+      {
+        headers: {
+          'X-Naver-Client-Id': process.env.NEXT_PUBLIC_CLIENT_ID,
+          'X-Naver-Client-Secret': process.env.NEXT_PUBLIC_CLIENT_SECRET,
+        },
+        params: {
+          query: query.q,
+          display: 20,
+          start: 1,
+          sort: 'sim',
+        },
       },
-      params: {
-        query: query.q,
-        display: 20,
-        start: 1,
-        sort: 'sim',
-      },
-    },
-  )
-  res.status(200).json(apiResponse.data)
+    )
+    res.status(200).json(apiResponse.data)
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ message: 'Internal Server Error' })
+  }
 }
