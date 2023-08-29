6fa465ac923e618e9a1c21734e7f55bbf4024991
Hchan2 <seongchan_@naver.com>
2023-08-29 22:23:02 +0900

    40 이상 시 20으로 수정에서 +10 으로 변경, Button에서 조건으로 disable

--- quick-news/pages/hooks/useMoreNews.ts
+++ quick-news/pages/hooks/useMoreNews.ts
@@ -4,10 +4,7 @@ export default function useMoreNews(initialPageSize = 20) {
   const [pageSize, setPageSize] = useState(initialPageSize)
 
   const handleLoadMore = () => {
-    setPageSize((prevPageSize) => {
-      const newPageSize = prevPageSize < 40 ? prevPageSize + 10 : 20
-      return newPageSize
-    })
+    setPageSize((prevPageSize) => prevPageSize + 10)
   }
 
   return { pageSize, handleLoadMore }
