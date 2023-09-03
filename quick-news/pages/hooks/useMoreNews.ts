743abf28b2c2c0f960d13c89adb9a51207af31f7
Hchan2 <seongchan_@naver.com>
2023-09-03 16:38:54 +0900

    더보기 최대 일 경우 비활성화

--- quick-news/pages/hooks/useMoreNews.ts
+++ quick-news/pages/hooks/useMoreNews.ts
@@ -1,11 +1,19 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { MAX_PAGE_COUNT } from '../../constants/CommonVariable'
 
 export default function useMoreNews(initialPageSize = 20) {
   const [pageSize, setPageSize] = useState(initialPageSize)
+  const [isAllLoaded, setIsAllLoaded] = useState(false)
 
   const handleLoadMore = () => {
     setPageSize((prevPageSize) => prevPageSize + 10)
   }
 
-  return { pageSize, handleLoadMore }
+  useEffect(() => {
+    if (pageSize >= MAX_PAGE_COUNT) {
+      setIsAllLoaded(true)
+    }
+  }, [pageSize])
+
+  return { pageSize, handleLoadMore, isAllLoaded }
 }
