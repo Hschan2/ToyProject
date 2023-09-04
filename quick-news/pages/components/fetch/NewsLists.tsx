55843ef9f740a788aa3a5ba838c3daf7bb977107
Hchan2 <seongchan_@naver.com>
2023-09-04 22:22:43 +0900

    item 요소 분리

--- quick-news/pages/components/fetch/NewsLists.tsx
+++ quick-news/pages/components/fetch/NewsLists.tsx
@@ -1,10 +1,5 @@
 import { Suspense, lazy, useRef } from 'react'
-import Link from 'next/link'
-import moment from 'moment'
 import 'moment/locale/ko'
-import { v4 as uuidv4 } from 'uuid'
-import { NewsCard } from '../../../styles/NewsStyle'
-import { DateTime } from '../../../styles/InfoStyle'
 import useVisibility from '../../hooks/useVisibility'
 import useMoreNews from '../../hooks/useMoreNews'
 import MoreViewButton from '../btn/MoreViewButton'
@@ -12,9 +7,10 @@ import { MAX_PAGE_COUNT } from '../../../constants/CommonVariable'
 import NaverNewsFetch from './NaverNewsFetch'
 
 const Loading = lazy(() => import('../page/Loading'))
+const NewsItem = lazy(() => import('../page/NewsItem'))
 
 export default function NewsLists() {
-  const { pageSize, handleLoadMore } = useMoreNews()
+  const { pageSize, handleLoadMore, isAllLoaded } = useMoreNews()
   const newsListRef = useRef<HTMLDivElement | null>(null)
   const isVisible = useVisibility(newsListRef)
   const { visibleNews, isLoading } = NaverNewsFetch(pageSize)
@@ -22,22 +18,9 @@ export default function NewsLists() {
   return (
     <Suspense fallback={<Loading />}>
       <div ref={newsListRef}>
-        {visibleNews?.map(
-          (item) =>
-            isVisible && (
-              <Link href={item.link} target="_blank" key={uuidv4()}>
-                <NewsCard>
-                  <h3 dangerouslySetInnerHTML={{ __html: item.title }} />
-                  <DateTime>
-                    {moment(item.pubDate).format('YYYY-MM-DD HH:mm')}
-                  </DateTime>
-                  <p dangerouslySetInnerHTML={{ __html: item.description }} />
-                </NewsCard>
-              </Link>
-            ),
-        )}
+        {visibleNews?.map((item) => isVisible && <NewsItem item={item} />)}
       </div>
-      {!isLoading && (
+      {!isAllLoaded && !isLoading && (
         <MoreViewButton
           onClick={handleLoadMore}
           disabled={isLoading || pageSize >= MAX_PAGE_COUNT}
---
+++ quick-news/pages/components/page/NewsItem.tsx
@@ -0,0 +1,21 @@
+import moment from 'moment'
+import Link from 'next/link'
+import React from 'react'
+import { v4 as uuidv4 } from 'uuid'
+import { NewsCard } from '../../../styles/NewsStyle'
+import { DateTime } from '../../../styles/InfoStyle'
+import { NewsContent } from '../../../interfaces/Interfaces'
+
+function NewsItem({ item }: NewsContent) {
+  return (
+    <Link href={item.link} target="_blank" key={item.id}>
+      <NewsCard>
+        <h3 dangerouslySetInnerHTML={{ __html: item.title }} />
+        <DateTime>{moment(item.pubDate).format('YYYY-MM-DD HH:mm')}</DateTime>
+        <p dangerouslySetInnerHTML={{ __html: item.description }} />
+      </NewsCard>
+    </Link>
+  )
+}
+
+export default React.memo(NewsItem)
