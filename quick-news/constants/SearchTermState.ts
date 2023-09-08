6ceaff1b15b9ed18c6ca83e9007610f85c87a239
Hchan2 <seongchan_@naver.com>
2023-09-08 19:09:07 +0900

    검색 후 새로고침 시 데이터 사라지는 문제 → recoil-persist로 해결

--- quick-news/constants/SearchTermState.ts
+++ quick-news/constants/SearchTermState.ts
@@ -1,6 +1,10 @@
 import { atom } from 'recoil'
+import { recoilPersist } from 'recoil-persist'
+
+const { persistAtom } = recoilPersist()
 
 export const searchState = atom<string>({
   key: 'searchTermState',
   default: '',
+  effects_UNSTABLE: [persistAtom],
 })
