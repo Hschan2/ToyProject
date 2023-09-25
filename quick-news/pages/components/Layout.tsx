0d13a0f36a64f83b080e6bab70c5af8715b7d5c1
Hchan2 <seongchan_@naver.com>
2023-09-25 22:33:27 +0900

    초기 접근, 새로고침 시 검색버튼 미리 불러오기 (스타일 적용을 위해)

--- quick-news/pages/components/Layout.tsx
+++ quick-news/pages/components/Layout.tsx
@@ -1,11 +1,14 @@
+import dynamic from 'next/dynamic'
 import React, { Suspense, lazy } from 'react'
 
 const LazyMoveUp = lazy(() => import('./btn/MoveUp'))
 const LazyNavbar = lazy(() => import('./nav/Navbar'))
-const LazySearchButton = lazy(() => import('./btn/SearchButton'))
 const LazyTimelineScroll = lazy(() => import('./page/TimelineScroll'))
 const LazyNotificationModal = lazy(() => import('./page/NotificationModal'))
 const LazyLoading = lazy(() => import('./page/Loading'))
+const DynamicSearchButton = dynamic(() => import('./btn/SearchButton'), {
+  ssr: false,
+})
 
 export default function Layout({ children }: { children: JSX.Element }) {
   return (
@@ -13,7 +16,7 @@ export default function Layout({ children }: { children: JSX.Element }) {
       <LazyNavbar />
       <div>{children}</div>
       <LazyMoveUp />
-      <LazySearchButton />
+      <DynamicSearchButton />
       <LazyTimelineScroll />
       <LazyNotificationModal />
     </Suspense>
