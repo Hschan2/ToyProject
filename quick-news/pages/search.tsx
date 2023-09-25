e74cb8789f340ccb82cf8564cd1a4402049d8cd6
Hchan2 <seongchan_@naver.com>
2023-09-25 22:32:15 +0900

    검색 Recoil을 제거함으로써 Params으로 페이지 Title 적용

--- quick-news/pages/search.tsx
+++ quick-news/pages/search.tsx
@@ -1,16 +1,17 @@
+import { useSearchParams } from 'next/navigation'
 import { lazy } from 'react'
 import { useRecoilValue } from 'recoil'
-import { searchState } from '../constants/SearchTermState'
 
 const LazySearchNews = lazy(() => import('./components/page/SearchNews'))
 const LazyContents = lazy(() => import('./components/Contents'))
 
 export default function Search() {
-  const searchTerm = useRecoilValue(searchState)
+  const searchParams = useSearchParams()
+  const searchedValue = searchParams.get('q') || ''
 
   return (
     <LazyContents
-      title={`검색: ${searchTerm}`}
+      title={`검색 결과: ${searchedValue}`}
       description="검색 결과를 확인하세요"
     >
       <LazySearchNews />
