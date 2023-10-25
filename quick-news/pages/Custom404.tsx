f932990ef967f92aa9b0875cacf6e58b515e7f06
Hchan2 <seongchan_@naver.com>
2023-10-25 14:21:45 +0900

    NextJS의 Image 라이브러리를 활용해 이미지 최적화

--- quick-news/pages/Custom404.tsx
+++ quick-news/pages/Custom404.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import { Wrapper } from '../styles/PageStyle'
 
 export default function Custom404({
@@ -9,8 +10,10 @@ export default function Custom404({
 }) {
   return (
     <Wrapper>
-      <img
+      <Image
         src="https://png.pngtree.com/png-vector/20210827/ourmid/pngtree-error-404-page-not-found-png-image_3832696.jpg"
+        width={400}
+        height={400}
         loading="lazy"
         alt={`${statusCode}, ${message}`}
       />
