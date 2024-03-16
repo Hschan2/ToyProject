ef71bcc5737612ac7049827571aa21600a88fa46
Hchan2 <seongchan_@naver.com>
2024-03-16 23:26:21 +0900

    fix: newsApi 코드 수정

--- quick-news/pages/components/fetch/CreateApi.ts
+++ quick-news/pages/components/fetch/CreateApi.ts
@@ -1,18 +1,20 @@
 import { MAX_PAGE_COUNT } from '@/constants/CommonVariable'
-import { CategoryNewsLists } from '@/interfaces/interface'
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 const fromToday = new Date().toISOString().split('T')[0]
 
-export const newsCreateApi = createApi({
-  reducerPath: 'api',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://newsapi.org/v2/' }),
+export const newsApi = createApi({
+  reducerPath: 'newsApi',
+  baseQuery: fetchBaseQuery({ baseUrl: '/' }),
   endpoints: (builder) => ({
-    fetchNews: builder.query<CategoryNewsLists, string | undefined>({
-      query: (newCategory) =>
-        `top-headlines?country=kr&from=${fromToday}&pageSize=${MAX_PAGE_COUNT}&category=${newCategory}&apiKey=${process.env.NEXT_PUBLIC_NEWS_API_KEY}`,
+    fetchNews: builder.query({
+      query: (newCategory: string | undefined) => ({
+        url: `https://newsapi.org/v2/top-headlines?country=kr&from=${fromToday}&pageSize=${MAX_PAGE_COUNT}&apiKey=${
+          process.env.NEXT_PUBLIC_NEWS_API_KEY
+        }${newCategory ? `&category=${newCategory}` : ''}`,
+      }),
     }),
   }),
 })
 
-export const { useFetchNewsQuery } = newsCreateApi
+export const { useFetchNewsQuery } = newsApi
