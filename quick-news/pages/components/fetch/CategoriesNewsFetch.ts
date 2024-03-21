b4157652e9ec12242c82b2b34e65e4e1d973bcda
Hchan2 <seongchan_@naver.com>
2024-03-21 22:11:44 +0900

    fix: 주석 처리 코드 제거 및 불필요한 코드 제거

--- quick-news/pages/components/fetch/CategoriesNewsFetch.ts
+++ quick-news/pages/components/fetch/CategoriesNewsFetch.ts
@@ -1,35 +1,10 @@
-import axios from 'axios'
-import { CategoryNewsLists } from '../../../interfaces/interface'
-import { MAX_PAGE_COUNT } from '../../../constants/CommonVariable'
-import { newsApi, useFetchNewsQuery } from './CreateApi'
+import { useFetchNewsQuery } from './CreateApi'
 import { toast } from 'react-toastify'
-
-const fetchNews = async (newCategory: string | undefined) => {
-  const fromToday = new Date().toISOString().split('T')[0]
-
-  let url = `https://newsapi.org/v2/top-headlines?country=kr&from=${fromToday}&pageSize=${MAX_PAGE_COUNT}&apiKey=${process.env.NEXT_PUBLIC_NEWS_API_KEY}`
-  if (newCategory) {
-    url += `&category=${newCategory}`
-  }
-
-  const response = await axios.get<CategoryNewsLists>(url)
-
-  return response.data.articles
-}
 
 export default function CategoriesNewsFetch(
   category: string | undefined,
   pageSize: number,
 ) {
-  // const { data: articles, isLoading } = useQuery(
-  //   ['news', category],
-  //   () => fetchNews(category),
-  //   {
-  //     refetchOnWindowFocus: false,
-  //     cacheTime: 30 * 60 * 1000,
-  //   },
-  // )
-
   const { data: articles, isLoading, error } = useFetchNewsQuery(category)
 
   if (error) {
