51a3daf74458718ddeb2046cc93c2fff260cd442
Hchan2 <seongchan_@naver.com>
2024-03-27 22:11:11 +0900

    fix: lightBackground, lightText, shadow 스타일 추가

--- quick-news/constants/StyleVariable.ts
+++ quick-news/constants/StyleVariable.ts
@@ -1,14 +1,20 @@
 export const COMMON_COLOR = '#4d7653'
 export const LIGHT_THEME = {
   background: '#FAF8F6',
+  lightBackground: '#FAF8F3',
   text: '#111111',
-  border: '#111111',
-  hover: '#FFFCFA',
+  lightText: '#8C8C8C',
+  border: '#D5D5D5',
+  shadow:
+    'rgba(50, 50, 93, 0.25) 0px 3.125rem 6.25rem -1.25rem, rgba(0, 0, 0, 0.3) 0px 1.875rem 3.75rem -1.875rem',
 }
 
 export const DARK_THEME = {
   background: '#000',
+  lightBackground: '#080808',
   text: '#FAF8F6',
-  border: '#FAF8F6',
-  hover: '#111',
+  lightText: '#8C8C8C',
+  border: '#4C4C4C',
+  shadow:
+    'rgba(205, 205, 162, 0.4) 0px 3.125rem 6.25rem -1.25rem, rgba(0, 0, 0, 0.3) 0px 1.875rem 3.75rem -1.875rem',
 }
