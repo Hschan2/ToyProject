f010522fbdbd5db704c5c6f12bb8dd422d4aeebd
Hchan2 <seongchan_@naver.com>
2024-03-28 20:41:02 +0900

    fix: 전체적인 색 조정

--- quick-news/constants/StyleVariable.ts
+++ quick-news/constants/StyleVariable.ts
@@ -1,20 +1,20 @@
 export const COMMON_COLOR = '#4d7653'
 export const LIGHT_THEME = {
-  background: '#FAF8F6',
-  lightBackground: '#FAF8F3',
+  background: '#f6f6f6',
+  lightBackground: '#f5f5f5',
   text: '#111111',
-  lightText: '#8C8C8C',
-  border: '#D5D5D5',
+  lightText: '#6a6a6a',
+  border: '#353839',
   shadow:
     'rgba(50, 50, 93, 0.25) 0px 3.125rem 6.25rem -1.25rem, rgba(0, 0, 0, 0.3) 0px 1.875rem 3.75rem -1.875rem',
 }
 
 export const DARK_THEME = {
-  background: '#000',
-  lightBackground: '#080808',
-  text: '#FAF8F6',
-  lightText: '#8C8C8C',
-  border: '#4C4C4C',
+  background: '#2d2d2d',
+  lightBackground: '#373737',
+  text: '#fff',
+  lightText: '#a9a9a9',
+  border: '#bdbdbd',
   shadow:
     'rgba(205, 205, 162, 0.4) 0px 3.125rem 6.25rem -1.25rem, rgba(0, 0, 0, 0.3) 0px 1.875rem 3.75rem -1.875rem',
 }
