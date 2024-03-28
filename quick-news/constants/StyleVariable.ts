fc5ec518590d3882c98440a49385a6a36bb08edb
Hchan2 <seongchan_@naver.com>
2024-03-28 22:28:13 +0900

    fix: lightBackground 색상 수정

--- quick-news/constants/StyleVariable.ts
+++ quick-news/constants/StyleVariable.ts
@@ -1,10 +1,10 @@
 export const COMMON_COLOR = '#4d7653'
 export const LIGHT_THEME = {
-  background: '#f6f6f6',
-  lightBackground: '#f5f5f5',
+  background: '#fdfdfd',
+  lightBackground: '#f9f9f9',
   text: '#111111',
   lightText: '#6a6a6a',
-  border: '#353839',
+  border: '#dbdbdb',
   shadow:
     'rgba(50, 50, 93, 0.25) 0px 3.125rem 6.25rem -1.25rem, rgba(0, 0, 0, 0.3) 0px 1.875rem 3.75rem -1.875rem',
 }
@@ -14,7 +14,7 @@ export const DARK_THEME = {
   lightBackground: '#373737',
   text: '#fff',
   lightText: '#a9a9a9',
-  border: '#bdbdbd',
+  border: '#727272',
   shadow:
     'rgba(205, 205, 162, 0.4) 0px 3.125rem 6.25rem -1.25rem, rgba(0, 0, 0, 0.3) 0px 1.875rem 3.75rem -1.875rem',
 }
