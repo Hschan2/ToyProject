f0b54ac60d5c1ee5cd5f14b223cf0e703395e805
Hchan2 <seongchan_@naver.com>
2024-03-31 22:18:12 +0900

    fix: 뉴스 저장 이벤트(테스트)

--- quick-news/pages/components/page/NewsItem.tsx
+++ quick-news/pages/components/page/NewsItem.tsx
@@ -5,20 +5,24 @@ import { NewsCard, TitleSaveContainer } from '../../../styles/NewsStyle'
 import { DateTime } from '../../../styles/InfoStyle'
 import { NaverNewsList } from '../../../interfaces/interface'
 import { SaveButton } from '@/styles/ButtonStyle'
+import { SaveNews } from '../utils/storage'
 
-function NewsItem({ item }: NaverNewsList) {
-  const saveNews = () => {
+function NewsItem({ article }: NaverNewsList) {
+  const onSaveNews = () => {
+    SaveNews({ article })
   }
 
   return (
-    <Link href={item.link} target="_blank" key={item.id}>
+    <Link href={article.link} target="_blank" key={article.id}>
       <NewsCard>
         <TitleSaveContainer>
-          <h3 dangerouslySetInnerHTML={{ __html: item.title }} />
-          <SaveButton>저장</SaveButton>
+          <h3 dangerouslySetInnerHTML={{ __html: article.title }} />
+          <SaveButton onClick={onSaveNews}>저장</SaveButton>
         </TitleSaveContainer>
-        <DateTime>{moment(item.pubDate).format('YYYY-MM-DD HH:mm')}</DateTime>
-        <p dangerouslySetInnerHTML={{ __html: item.description }} />
+        <DateTime>
+          {moment(article.pubDate).format('YYYY-MM-DD HH:mm')}
+        </DateTime>
+        <p dangerouslySetInnerHTML={{ __html: article.description }} />
       </NewsCard>
     </Link>
   )
