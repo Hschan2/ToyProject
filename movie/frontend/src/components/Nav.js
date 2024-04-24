914aa385c34ee5127079b8c22a5b789a97f90150
Hchan2 <seongchan_@naver.com>
2024-04-24 21:50:49 +0900

    fix: Nav 스타일 바꾸기

--- movie/frontend/src/components/Nav.js
+++ movie/frontend/src/components/Nav.js
@@ -14,22 +14,18 @@ function Nav() {
     return (
         <div className={styles.navContainer}>
             <Link to="/">
-                <LottieFiles srcLink={MAIN_BANNER_LOTTIE} style={styles.img} />
+                <div className={styles.navTitle}>
+                    <LottieFiles srcLink={MAIN_BANNER_LOTTIE} style={styles.img} />
+                    <p className={styles.searchTitle}>Movie</p>
+                </div>
             </Link>
-            <nav>
-                <Link to="/" className={location.pathname === '/' ? styles.active : styles.nonActive}>
-                    <div className={styles.navMenus}>인기순</div>
-                </Link>
-                <Link to="/HighRated" className={location.pathname === '/HighRated' ? styles.active : styles.nonActive}>
-                    <div className={styles.navMenus}>평점순</div>
-                </Link>
-                <Link to="/NowPlaying" className={location.pathname === '/NowPlaying' ? styles.active : styles.nonActive}>
-                    <div className={styles.navMenus}>현재개봉작</div>
-                </Link>
-                <Link to="/Upcoming" className={location.pathname === '/Upcoming' ? styles.active : styles.nonActive}>
-                    <div className={styles.navMenus}>개봉예정작</div>
-                </Link>
-            </nav>
+            <div className={styles.searchContainer}>
+                <input className={styles.searchInput} type='text' placeholder='검색어를 입력하세요.' alt='검색' />
+                <button className={styles.searchBtn}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+                    <path fillRule="evenodd" d="M10.5 3.75a6.75 6.75 0 1 0 0 13.5 6.75 6.75 0 0 0 0-13.5ZM2.25 10.5a8.25 8.25 0 1 1 14.59 5.28l4.69 4.69a.75.75 0 1 1-1.06 1.06l-4.69-4.69A8.25 8.25 0 0 1 2.25 10.5Z" clipRule="evenodd" />
+                </svg>
+                </button>
+            </div>
         </div>
     )
 }
