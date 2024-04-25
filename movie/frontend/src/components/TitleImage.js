6224abe1445dce426291d6f8d2ba3a261e0f8a87
Hchan2 <seongchan_@naver.com>
2024-04-25 20:23:33 +0900

    fix: 로고 타이틀 컴포넌트

---
+++ movie/frontend/src/components/TitleImage.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import styles from '../style/navStyle.module.css';
+import LottieFiles from './lottieFiles'
+import { MAIN_BANNER_LOTTIE } from './constants/LottiefilesSrc'
+
+function TitleImage() {
+    return (
+        <div>
+            <Link to="/">
+                <div className={styles.navTitle}>
+                    <LottieFiles srcLink={MAIN_BANNER_LOTTIE} style={styles.img} />
+                    <p className={styles.searchTitle}>MOVIE</p>
+                </div>
+            </Link>
+        </div>
+    )
+}
+
+export default TitleImage
