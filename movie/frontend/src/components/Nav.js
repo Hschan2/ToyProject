43ce9ea0d0735d0a0bf0a269cafb279759cbab0f
Hchan2 <seongchan_@naver.com>
2024-04-28 22:14:45 +0900

    fix: Recoil 변수 사용하여 다크모드 설정

--- movie/frontend/src/components/Nav.js
+++ movie/frontend/src/components/Nav.js
@@ -1,21 +1,23 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styles from '../style/navStyle.module.css';
 import TitleImage from './TitleImage';
 import SearchBar from './SearchBar';
 import DarkModeBar from './DarkModeBar';
+import { useRecoilState } from 'recoil';
+import { darkModeState } from './constants/Store';
 
 /**
  * 상단 메뉴 컴포넌트
  * @returns 로고, 인기순, 평점순, 현재개봉작, 개봉예정작 메뉴 링크
  */
 function Nav() {
-    const [isDark, setIsDark] = useState(false);
+    const [isDarkMode, setIsDarkMode] = useRecoilState(darkModeState);
 
     return (
-        <div className={styles.navContainer} data-theme={isDark ? "dark" : "light"}>
+        <div className={styles.navContainer}>
             <TitleImage />
             <SearchBar />
-            <DarkModeBar isChecked={isDark} handleChange={() => setIsDark(!isDark)} />
+            <DarkModeBar isChecked={isDarkMode} handleChange={() => setIsDarkMode(prev => !prev)} />
         </div>
     )
 }
