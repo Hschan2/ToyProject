586098bf2b1a45fefd0bbe618e4fda507f0f89c2
Hchan2 <seongchan_@naver.com>
2024-05-02 22:34:00 +0900

    fix: ThemeProvider 설정

--- movie/frontend/src/App.js
+++ movie/frontend/src/App.js
@@ -5,24 +5,37 @@ import SlideItem from './components/pages/content/SlideItem';
 import { useRecoilState } from 'recoil';
 import { darkModeState } from './components/constants/Store';
 import Nav from './components/pages/nav/Nav';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyles } from '@mui/material';
 
 const Loading = lazy(() => import('./components/pages/loading/Loading'));
 const ToTop = lazy(() => import('./components/pages/scroll/ToTop'));
+
+const lightTheme = {
+  body: '#FFF',
+}
+
+const darkTheme = {
+  body: '#111',
+}
 
 function App() {
   const [isDarkMode, setIsDarkMode] = useRecoilState(darkModeState);
 
   return (
-    <div data-theme={isDarkMode ? "dark" : "light"}>
-      <Nav />
-      <Suspense fallback={<Loading />}>
-        <MainContent />
-        <SlideItem title="인기영화" />
-        {/* <SlideMenus /> */}
-      </Suspense>
-      {/* <Footer /> */}
-      <ToTop />
-    </div>
+    <ThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
+      <>
+        <GlobalStyles />
+        <Nav />
+        <Suspense fallback={<Loading />}>
+          <MainContent />
+          <SlideItem title="인기영화" />
+          {/* <SlideMenus /> */}
+        </Suspense>
+        {/* <Footer /> */}
+        <ToTop />
+      </>
+    </ThemeProvider>
   );
 }
 
