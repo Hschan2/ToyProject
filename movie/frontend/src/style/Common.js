7b2ef068c78e92a041ee9406204d6741fe118d58
Hchan2 <seongchan_@naver.com>
2024-05-03 16:15:19 +0900

    feat: 다크모드에 사용되는 색상들 선언

---
+++ movie/frontend/src/style/Common.js
@@ -0,0 +1,21 @@
+export const LIGHT_THEME = {
+    background: '#FFF',
+    lightBackground: '#f9f9f9',
+    text: '#111',
+    lightText: '#6a6a6a',
+    border: '#111',
+    shadow:
+        'rgba(50, 50, 93, 0.25) 0px 3.125rem 6.25rem -1.25rem, rgba(0, 0, 0, 0.3) 0px 1.875rem 3.75rem -1.875rem',
+    mainColor: '#4D7653',
+}
+
+export const DARK_THEME = {
+    background: '#111',
+    lightBackground: '#373737',
+    text: '#FFF',
+    lightText: '#a9a9a9',
+    border: '#FFF',
+    shadow:
+        'rgba(205, 205, 162, 0.4) 0px 3.125rem 6.25rem -1.25rem, rgba(0, 0, 0, 0.3) 0px 1.875rem 3.75rem -1.875rem',
+    mainColor: '#6FA677',
+}
