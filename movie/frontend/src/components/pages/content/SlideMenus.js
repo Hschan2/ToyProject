22de340e272a74b0f3897acdc52cb1fd36bb332c
Hchan2 <seongchan_@naver.com>
2024-05-07 21:43:54 +0900

    fix: 테스트 값에서 실제 데이터로 변경해보기

--- movie/frontend/src/components/pages/content/SlideMenus.js
+++ movie/frontend/src/components/pages/content/SlideMenus.js
@@ -3,17 +3,13 @@ import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import { CardDescription, CardTitle, CarouselButton, CarouselContainer, ContentCard, SlideCard, SlideImage } from '../../../style/Carousel';
+import { EMPTY_BACKGROUND_IMAGE } from '../../constants/variable';
+import GetData from '../../api/GetData';
 
 function SlideMenus() {
     const [currentSlide, setCurrentSlide] = useState(0);
     const sliderRef = useRef(null);
-
-    const testData = Array.from({ length: 10 }, (_, index) => ({
-        id: index + 1,
-        title: `Slide ${index + 1}`,
-        description: `This is slide number ${index + 1}`,
-        image: `https://via.placeholder.com/400x200?text=Slide${index + 1}`,
-    }));
+    const getMovieData = GetData();
 
     const settings = {
         dots: false,
@@ -63,7 +59,7 @@ function SlideMenus() {
     return (
         <CarouselContainer>
             <Slider ref={sliderRef} {...settings} afterChange={handleSlideChange}>
-                {testData.map((slide, index) => (
+                {getMovieData.map((slide, index) => (
                     <Slide key={index} {...slide} />
                 ))}
             </Slider>
@@ -74,7 +70,7 @@ function SlideMenus() {
                     </svg>
                 </CarouselButton>
             )}
-            {currentSlide !== testData.length - 1 && (
+            {currentSlide !== getMovieData.length - 1 && (
                 <CarouselButton className="right" onClick={handleNext}>
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
                         <path fillRule="evenodd" d="M16.28 11.47a.75.75 0 0 1 0 1.06l-7.5 7.5a.75.75 0 0 1-1.06-1.06L14.69 12 7.72 5.03a.75.75 0 0 1 1.06-1.06l7.5 7.5Z" clipRule="evenodd" />
@@ -89,7 +85,7 @@ export default SlideMenus
 
 const Slide = ({ image, title, description }) => (
     <SlideCard>
-        <SlideImage src={image} alt='Movie Image' loading='lazy' />
+        <SlideImage src={image ?? EMPTY_BACKGROUND_IMAGE} alt='Movie Image' loading='lazy' />
         <ContentCard>
             <CardTitle>{title}</CardTitle>
             <CardDescription>{description}</CardDescription>
