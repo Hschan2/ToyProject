9576c6dbc41b1b4d660e5051a4413de80bdd494c
Hchan2 <seongchan_@naver.com>
2024-05-11 22:15:43 +0900

    fix: 카테고리별 Fetch 값 선언

---
+++ movie/frontend/src/constants/api.js
@@ -0,0 +1,24 @@
+export const API_URL = [
+    {
+        id: 1,
+        title: '인기 영화',
+        url: 'http://localhost:8080/api/movies/popular'
+    },
+    {
+        id: 2,
+        title: '상영 영화',
+        url: 'http://localhost:8080/api/movies/now_playing'
+    },
+    {
+        id: 3,
+        title: '높은 평가 영화',
+        url: 'http://localhost:8080/api/movies/highRated'
+    },
+    {
+        id: 4,
+        title: '개봉예정 영화',
+        url: 'http://localhost:8080/api/movies/upcoming'
+    }
+]
+
+export const LATEST_MOVIE_URL = "http://localhost:8080/api/movies/highRated";
