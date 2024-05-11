ef890d6f95fd38d8cb4374cc7d04556a04015c00
Hchan2 <seongchan_@naver.com>
2024-05-11 22:18:17 +0900

    fix: 메인 콘텐츠 데이터 변경 및 react-query 적용

--- movie/frontend/src/components/pages/content/MainContent.js
+++ movie/frontend/src/components/pages/content/MainContent.js
@@ -1,17 +1,42 @@
 import React from 'react'
-import { BackImage, Container, Description, ImageContainer, Overlay, TextContainer } from '../../../style/MainContent';
+import { BackImage, Container, Description, DetailButton, ImageContainer, Overlay, SmallImage, TextContainer } from '../../../style/MainContent';
+import { EMPTY_BACKGROUND_IMAGE } from '../../constants/variable';
+import { Link } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
+import QueryMovie from '../../api/QueryMovie';
 
 function MainContent() {
+    const url = 'http://localhost:8080/api/movies/recommendation';
+    const { status, data, error, isFetching } = useQuery({
+        queryKey: ['movieLists', url],
+        queryFn: () => QueryMovie(url)
+    });
+
+    const movieImage = data && data.length > 0 ? `https://image.tmdb.org/t/p/w500/${data[0]?.poster_path}` : EMPTY_BACKGROUND_IMAGE;
+
     return (
         <Container>
-            <ImageContainer>
-                <Overlay></Overlay>
-                <BackImage src='/logo512.png' alt='Main Image' loading='lazy' />
-                <TextContainer>
-                    <h2>이미지 위에 텍스트</h2>
-                    <Description>추가할 텍스트 내용</Description>
-                </TextContainer>
-            </ImageContainer>
+            {data && data.length > 0 && (
+                <ImageContainer>
+                    <Overlay></Overlay>
+                    <BackImage src={movieImage} alt='Main Image' loading='lazy' />
+                    <TextContainer>
+                        <h2>{data[0]?.title}</h2>
+                        <Description>{data[0]?.overview}</Description>
+                        <Link to={`/Detail/${data[0]?.title}/${data[0]?.id}`}
+                            state={{ mTitle: data[0]?.title, id: data[0]?.id }}
+                            key={data[0]?.id}>
+                            <DetailButton>
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+                                    <path fillRule="evenodd" d="M4.5 5.653c0-1.427 1.529-2.33 2.779-1.643l11.54 6.347c1.295.712 1.295 2.573 0 3.286L7.28 19.99c-1.25.687-2.779-.217-2.779-1.643V5.653Z" clipRule="evenodd" />
+                                </svg>
+                                자세히 보기
+                            </DetailButton>
+                        </Link>
+                    </TextContainer>
+                    <SmallImage src={movieImage} alt='Main Image' loading='lazy' />
+                </ImageContainer>
+            )}
         </Container>
     )
 }
