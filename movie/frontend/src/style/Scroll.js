d9d1c2cb7720fb9c5bfa3843e9b8d61ab7a505fe
Hchan2 <seongchan_@naver.com>
2024-05-11 22:14:25 +0900

    fix: 위로 이동 버튼 개선

--- movie/frontend/src/style/Scroll.js
+++ movie/frontend/src/style/Scroll.js
@@ -4,7 +4,6 @@ export const UpTopButton = styled.button`
     position: fixed;
     bottom: 50px;
     right: 50px;
-    font-size: 30px;
     background: tomato;
     color: white;
     cursor: pointer;
@@ -12,5 +11,20 @@ export const UpTopButton = styled.button`
     height: 50px;
     border-radius: 50%;
     border: none;
-    box-shadow: 0 5px 10px #ccc;
+    transition: 0.3s;
+
+    svg {
+        width: 30px;
+        height: 30px;
+    }
+
+    &:hover {
+        background: #FF7547;
+    }
+`
+
+export const ContentContainer = styled.div`
+    height: auto;
+    min-height: 100%;
+    padding-bottom: 30px;
 `
