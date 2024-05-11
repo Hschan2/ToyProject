73872ad9ed0d806e0f45b9a8bc4f501bb1b8435a
Hchan2 <seongchan_@naver.com>
2024-05-11 22:14:54 +0900

    fix: 흰 화면 이미지 변경

--- movie/frontend/src/components/constants/variable.js
+++ movie/frontend/src/components/constants/variable.js
@@ -1,1 +1,1 @@
-export const EMPTY_BACKGROUND_IMAGE = "https://t3.ftcdn.net/jpg/02/44/17/82/360_F_244178265_NP4S8WdlZRGYVSkVkxhtiDonSfQPAbyO.jpg";
+export const EMPTY_BACKGROUND_IMAGE = 'https://via.placeholder.com/500x500';
