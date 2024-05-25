c425171025ef810eca690c3ff24265a6cd232d86
Hchan2 <seongchan_@naver.com>
2024-05-25 22:54:18 +0900

    feat: 검색 페이지 생성

---
+++ movie/frontend/src/pages/Search.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { useLocation } from 'react-router'
+import SEO from '../components/pages/seo/SEO';
+import { QuerySearchMovie } from '../components/api/QueryMovie';
+import { useQuery } from '@tanstack/react-query';
+import MovieLists from '../components/pages/content/MovieLists';
+
+function Search() {
+    const location = useLocation();
+    const queryParams = new URLSearchParams(location.search);
+    const query = queryParams.get('query');
+
+    const { status, data, error, isFetching } = useQuery({
+        queryKey: ['searchResults', query],
+        queryFn: () => QuerySearchMovie(query),
+        enabled: !!query,
+    });
+
+    if (status === 'loading' || isFetching) {
+        return <div>Loading...</div>;
+    }
+
+    if (status === 'error') {
+        return <div>Error: {error.message}</div>;
+    }
+
+    return (
+        <div>
+            <SEO title={query} />
+            <h2>검색 결과 | {query}</h2>
+            <MovieLists movieList={data} />
+        </div>
+    )
+}
+
+export default Search
