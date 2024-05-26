dbfe7315b16c30b5f2bd8dee489fc90641bc2f14
Hchan2 <seongchan_@naver.com>
2024-05-26 22:32:55 +0900

    fix: React-Slick의 화살표 버튼 제거

--- movie/frontend/src/components/pages/content/MainContent.js
+++ movie/frontend/src/components/pages/content/MainContent.js
@@ -18,6 +18,8 @@ function MainContent() {
         speed: 500,
         slidesToShow: 1,
         slidesToScroll: 1,
+        prevArrow: <></>,
+        nextArrow: <></>,
         autoplay: true,
         autoplaySpeed: 10000,
     };
@@ -42,7 +44,7 @@ function MainContent() {
                                 <TextContainer>
                                     <h2>{movie.title}</h2>
                                     <Description>{movie.overview}</Description>
-                                    <Link to={`/detail/${movie.title}/${movie.id}`} state={{ mTitle: movie.title, id: movie.id }}>
+                                    <Link to={`/detail/${movie.id}`}>
                                         <DetailButton>
                                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
                                                 <path fillRule="evenodd" d="M4.5 5.653c0-1.427 1.529-2.33 2.779-1.643l11.54 6.347c1.295.712 1.295 2.573 0 3.286L7.28 19.99c-1.25.687-2.779-.217-2.779-1.643V5.653Z" clipRule="evenodd" />
