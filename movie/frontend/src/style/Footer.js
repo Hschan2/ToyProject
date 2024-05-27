1fd34e52a12df1b469912bb4eb00c88841ca65e4
Hchan2 <seongchan_@naver.com>
2024-05-27 20:42:01 +0900

    fix: Footer 스타일 변경

--- movie/frontend/src/style/Footer.js
+++ movie/frontend/src/style/Footer.js
@@ -1,10 +1,9 @@
 import { styled } from "styled-components";
 
 export const FooterContainer = styled.div`
-    height: 30px;
-    position: relative;
-    transform: translateY(0%);
-    border-top: 1px solid #D7D7D7;
+    border-top: 1px solid ${(props) => props.theme.lightBorder};
+    margin-top: 24px;
+    bottom: 0;
 `
 
 export const FooterContents = styled.div`
@@ -12,7 +11,7 @@ export const FooterContents = styled.div`
     flex-direction: column;
     align-items: center;
     justify-content: center;
-    color: #BFBFBF;
+    color: ${(props) => props.theme.lightText};
     font-size: 12px;
     padding: 10px 0;
 `
