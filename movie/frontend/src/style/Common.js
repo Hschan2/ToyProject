678707f350afac70cee00aa3f4388d5ea5528ad4
Hchan2 <seongchan_@naver.com>
2024-05-27 20:42:37 +0900

    fix: 다크모드 시, 연한 색상 코드 변경

--- movie/frontend/src/style/Common.js
+++ movie/frontend/src/style/Common.js
@@ -4,6 +4,7 @@ export const LIGHT_THEME = {
     text: '#111',
     lightText: '#6a6a6a',
     border: '#111',
+    lightBorder: '#EAEAEA',
     shadow:
         'rgba(50, 50, 93, 0.25) 0px 3.125rem 6.25rem -1.25rem, rgba(0, 0, 0, 0.3) 0px 1.875rem 3.75rem -1.875rem',
     mainColor: '#4D7653',
@@ -13,8 +14,9 @@ export const DARK_THEME = {
     background: '#111',
     lightBackground: '#373737',
     text: '#FFF',
-    lightText: '#a9a9a9',
+    lightText: '#BDBDBD',
     border: '#FFF',
+    lightBorder: '#4C4C4C',
     shadow:
         'rgba(205, 205, 162, 0.4) 0px 3.125rem 6.25rem -1.25rem, rgba(0, 0, 0, 0.3) 0px 1.875rem 3.75rem -1.875rem',
     mainColor: '#6FA677',
