20e693855793a4101614d78b470be764a65cf174
Hchan2 <seongchan_@naver.com>
2024-05-30 22:21:02 +0900

    feat: 스켈레톤 디자인 추가

--- movie/frontend/src/style/Loading.js
+++ movie/frontend/src/style/Loading.js
@@ -1,4 +1,4 @@
-import { styled } from "styled-components";
+import { keyframes, styled } from "styled-components";
 import LottieFiles from "../components/pages/animation/lottieFiles";
 
 export const LoadingContainer = styled.div`
@@ -20,3 +20,29 @@ export const Message = styled.p`
   font-weight: bold;
   user-select: none;
 `;
+
+export const Shimmer = keyframes`
+  0% {
+    background-position: -468px 0;
+  }
+
+  100% {
+      background-position: 468px 0;
+  }
+`
+
+export const Skeleton = styled.div`
+  background: #E0E0E0;
+  border-radius: 4px;
+  width: ${(props) => props.width};
+  height: ${(props) => props.height};
+  animation: ${Shimmer} 1.5s infinite linear;
+  background: linear-gradient(to right, #E0E0E0 8%, #F0F0F0 18%, #E0E0E0 33%);
+  background-size: 800px 104px;
+  margin: 0 ${(props) => props.margin};
+`
+
+export const SkeletonLayout = styled.div`
+  display: flex;
+  flex-direction: row;
+`
