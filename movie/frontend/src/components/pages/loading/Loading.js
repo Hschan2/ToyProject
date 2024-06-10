b62df3b8fa717256d6c59163d82f41f1dbc02a4f
Hchan2 <seongchan_@naver.com>
2024-06-10 16:42:17 +0900

    fix: 불필요한 내용 제거

--- movie/frontend/src/components/pages/loading/Loading.js
+++ movie/frontend/src/components/pages/loading/Loading.js
@@ -1,6 +1,5 @@
 import React from 'react'
-import LottieFiles from '../animation/lottieFiles';
-import { ERROR_404, ERROR_500, LOADING_LOTTIE } from '../../constants/FileLink';
+import { LOADING_LOTTIE } from '../../constants/FileLink';
 import { LoadingContainer, LoadingAnimation, Message } from '../../../style/Loading';
 
 /**
@@ -11,23 +10,6 @@ function Loading() {
   return (
     <LoadingContainer>
       <LoadingAnimation srcLink={LOADING_LOTTIE} />
-      <Message>Loading...</Message>
-    </LoadingContainer>
-  )
-}
-
-export function Error404({ status }) {
-  return (
-    <LoadingContainer stat={status}>
-      <LoadingAnimation srcLink={ERROR_404} stat={status} />
-    </LoadingContainer>
-  )
-}
-
-export function Error500({ status }) {
-  return (
-    <LoadingContainer stat={status}>
-      <LoadingAnimation srcLink={ERROR_500} stat={status} />
     </LoadingContainer>
   )
 }
