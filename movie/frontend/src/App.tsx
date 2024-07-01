8b5bd1644af2f7952be8c984b1530cf976426cca
Hchan2 <seongchan_@naver.com>
2024-07-01 22:24:18 +0900

    fix: 코드 정리 및 searching 추가

--- movie/frontend/src/App.tsx
+++ movie/frontend/src/App.tsx
@@ -1,23 +1,29 @@
-import { lazy, Suspense } from 'react';
-import './App.css';
-import { useRecoilState } from 'recoil';
-import { darkModeState } from './components/constants/Store';
-import Nav from './components/pages/nav/Nav';
-import { ThemeProvider } from 'styled-components';
-import { DARK_THEME, LIGHT_THEME } from './style/ThemeStyle';
-import GlobalStyles from './style/GlobalStyles';
-import { API_URL } from './components/constants/api';
-import Footer from './components/pages/footer/Footer';
-import { Content, ContentContainer } from './style/Scroll';
-import { Route, Routes } from 'react-router';
-import SEO from './components/pages/seo/SEO';
-import Loading from './components/pages/loading/Loading';
-import ToUp from './components/pages/scroll/ToUp';
+import { lazy, Suspense } from "react";
+import "./App.css";
+import { useRecoilState } from "recoil";
+import { darkModeState } from "./components/constants/Store";
+import Nav from "./components/pages/nav/Nav";
+import { ThemeProvider } from "styled-components";
+import { DARK_THEME, LIGHT_THEME } from "./style/ThemeStyle";
+import GlobalStyles from "./style/GlobalStyles";
+import { API_URL } from "./components/constants/api";
+import Footer from "./components/pages/footer/Footer";
+import { Content, ContentContainer } from "./style/Scroll";
+import { Route, Routes } from "react-router";
+import SEO from "./components/pages/seo/SEO";
+import Loading from "./components/pages/loading/Loading";
+import ToUp from "./components/pages/scroll/ToUp";
+import BottomMenu from "./components/pages/scroll/BottomMenu";
+import Searching from "./components/pages/search/Searching";
 
-const MainContent = lazy(() => import('./components/pages/content/MainContent'));
-const SlideCategory = lazy(() => import('./components/pages/content/SlideCategory'));
-const Search = lazy(() => import('./pages/Search'));
-const Detail = lazy(() => import('./pages/Detail'));
+const MainContent = lazy(
+  () => import("./components/pages/content/MainContent")
+);
+const SlideCategory = lazy(
+  () => import("./components/pages/content/SlideCategory")
+);
+const Search = lazy(() => import("./pages/Search"));
+const Detail = lazy(() => import("./pages/Detail"));
 
 function App() {
   const [isDarkMode, setIsDarkMode] = useRecoilState(darkModeState);
@@ -31,20 +37,25 @@ function App() {
           <Content>
             <Suspense fallback={<Loading />}>
               <Routes>
-                <Route path="/" element={
-                  <>
-                    <SEO title="Home" />
-                    <MainContent />
-                    {API_URL?.map((categories) => (
-                      <SlideCategory key={categories.id} {...categories} />
-                    ))}
-                  </>
-                } />
+                <Route
+                  path="/"
+                  element={
+                    <>
+                      <SEO title="Home" />
+                      <MainContent />
+                      {API_URL?.map((categories) => (
+                        <SlideCategory key={categories.id} {...categories} />
+                      ))}
+                    </>
+                  }
+                />
                 <Route path="/search" element={<Search />} />
+                <Route path="/searching" element={<Searching />} />
                 <Route path="/detail/:id" element={<Detail />} />
               </Routes>
             </Suspense>
           </Content>
+          <BottomMenu />
           <Footer />
         </ContentContainer>
         <ToUp />
