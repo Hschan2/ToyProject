0c994d6f41126baea59532436353e376026e005b
Hchan2 <seongchan_@naver.com>
2024-07-08 22:11:30 +0900

    fix: 테마 색상 반전색 추가

--- quick-news/utils/ColorValue.ts
+++ quick-news/utils/ColorValue.ts
@@ -1,22 +1,24 @@
-export const COMMON_COLOR = '#4d7653'
+export const COMMON_COLOR = '#4D7653'
 export const LIGHT_THEME = {
-  background: '#fdfdfd',
-  lightBackground: '#f9f9f9',
+  background: '#FDFDFD',
+  lightBackground: '#F9F9F9',
   text: '#111111',
-  lightText: '#6a6a6a',
-  border: '#dbdbdb',
+  lightText: '#6A6A6A',
+  border: '#DBDBDB',
   shadow:
     'rgba(50, 50, 93, 0.25) 0px 3.125rem 6.25rem -1.25rem, rgba(0, 0, 0, 0.3) 0px 1.875rem 3.75rem -1.875rem',
   mainColor: '#4D7653',
+  reverseText: '#FFF',
 }
 
 export const DARK_THEME = {
-  background: '#2d2d2d',
+  background: '#2D2D2D',
   lightBackground: '#373737',
-  text: '#fff',
-  lightText: '#a9a9a9',
+  text: '#FFF',
+  lightText: '#A9A9A9',
   border: '#727272',
   shadow:
     'rgba(205, 205, 162, 0.4) 0px 3.125rem 6.25rem -1.25rem, rgba(0, 0, 0, 0.3) 0px 1.875rem 3.75rem -1.875rem',
   mainColor: '#6FA677',
+  reverseText: '#111111',
 }
