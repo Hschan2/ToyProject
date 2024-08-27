242a6c66925775d17f8924004eb44fc77b0e3041
Hchan2 <seongchan_@naver.com>
2024-08-27 21:59:12 +0900

    update: footer 색상 추가

--- quick-news/utils/ColorValue.ts
+++ quick-news/utils/ColorValue.ts
@@ -9,6 +9,7 @@ export const LIGHT_THEME = {
     'rgba(50, 50, 93, 0.25) 0px 3.125rem 6.25rem -1.25rem, rgba(0, 0, 0, 0.3) 0px 1.875rem 3.75rem -1.875rem',
   mainColor: '#4D7653',
   reverseText: '#FFF',
+  footer: '#333',
 }
 
 export const DARK_THEME = {
@@ -21,4 +22,5 @@ export const DARK_THEME = {
     'rgba(205, 205, 162, 0.4) 0px 3.125rem 6.25rem -1.25rem, rgba(0, 0, 0, 0.3) 0px 1.875rem 3.75rem -1.875rem',
   mainColor: '#6FA677',
   reverseText: '#111111',
+  footer: '#FFF',
 }
