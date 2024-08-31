66d743f4461253a28c906d1e41421e8ba1bb5fd7
Hchan2 <seongchan_@naver.com>
2024-08-31 21:14:38 +0900

    update: 타이틀 색상 추가

--- movie/frontend/src/style/ThemeStyle.ts
+++ movie/frontend/src/style/ThemeStyle.ts
@@ -8,6 +8,7 @@ export const LIGHT_THEME = {
     shadow:
         'rgba(50, 50, 93, 0.25) 0px 3.125rem 6.25rem -1.25rem, rgba(0, 0, 0, 0.3) 0px 1.875rem 3.75rem -1.875rem',
     mainColor: '#4D7653',
+    title: "#C8102E",
 }
 
 export const DARK_THEME = {
@@ -20,4 +21,5 @@ export const DARK_THEME = {
     shadow:
         'rgba(205, 205, 162, 0.4) 0px 3.125rem 6.25rem -1.25rem, rgba(0, 0, 0, 0.3) 0px 1.875rem 3.75rem -1.875rem',
     mainColor: '#6FA677',
+    title: "#FF6F6F",
 }
