3450413dfe4f569e9e84fb9ff6dc452252b55efd
Hchan2 <seongchan_@naver.com>
2024-09-01 21:20:27 +0900

    fix: dynamic 호출로 변경 및 loading 추가

--- quick-news/pages/news/NewsLists.tsx
+++ quick-news/pages/news/NewsLists.tsx
@@ -1,14 +1,16 @@
-import React, { lazy } from 'react'
+import React from 'react'
 import dynamic from 'next/dynamic'
 import 'moment/locale/ko'
 import useMoreNews from '../../utils/hooks/useMoreNews'
 import NaverNewsFetch from '../fetch/NaverNewsFetch'
 import useInfiniteScroll from '../../utils/hooks/useInfiniteScroll'
 import { CommonNewsListProps, NaverNewsProps } from '../../utils/types/type'
+import Loading from '../loading/Loading'
 
-const NewsItem = lazy(() => import('./NewsItem'))
+const NewsItem = dynamic(() => import('./NewsItem'), { ssr: false })
 const RenderNewsPage = dynamic(() => import('../page/render/RenderNewsPage'), {
   ssr: false,
+  loading: () => <Loading />,
 }) as React.ComponentType<CommonNewsListProps<NaverNewsProps>>
 
 export default function NewsLists() {
