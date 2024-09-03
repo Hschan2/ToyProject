3fc6e7f2324413ded2a27719c357653f35e98514
Hchan2 <seongchan_@naver.com>
2024-09-03 21:01:43 +0900

    update: 폰트 라이브러리 제거, svg 코드로 수정

--- quick-news/components/Button/SearchBtn.tsx
+++ quick-news/components/Button/SearchBtn.tsx
@@ -1,7 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { useRouter } from 'next/router'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSearch } from '@fortawesome/free-solid-svg-icons/faSearch'
 import {
   InputWrapper,
   SearchContainer,
@@ -61,7 +59,18 @@ export default function SearchButton() {
         onClick={handleToggleInput}
         aria-label="뉴스검색"
       >
-        <FontAwesomeIcon icon={faSearch} size="1x" />
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          className="size-6"
+        >
+          <path
+            fillRule="evenodd"
+            d="M10.5 3.75a6.75 6.75 0 1 0 0 13.5 6.75 6.75 0 0 0 0-13.5ZM2.25 10.5a8.25 8.25 0 1 1 14.59 5.28l4.69 4.69a.75.75 0 1 1-1.06 1.06l-4.69-4.69A8.25 8.25 0 0 1 2.25 10.5Z"
+            clipRule="evenodd"
+          />
+        </svg>
       </SearchingButton>
       {isInputVisible && (
         <InputWrapper>
