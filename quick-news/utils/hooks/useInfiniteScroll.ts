86f8a3fbb05cf2b1af903ef821951ead6f2fc056
Hchan2 <seongchan_@naver.com>
2024-09-09 22:31:23 +0900

    fix: setTimeout → IntersectionObserver로 변경

--- quick-news/utils/hooks/useInfiniteScroll.ts
+++ quick-news/utils/hooks/useInfiniteScroll.ts
@@ -1,34 +1,38 @@
-import { useCallback, useEffect, useRef } from 'react'
+import { useEffect, useRef } from 'react'
 import { InfiniteScrollProps } from '../types/type'
 
 export default function useInfiniteScroll({
   handleLoadMore,
   isAllLoaded,
 }: InfiniteScrollProps) {
-  const timeoutIdRef = useRef<NodeJS.Timeout | null>(null)
-
-  const handleScroll = useCallback(() => {
-    if (timeoutIdRef.current) {
-      clearTimeout(timeoutIdRef.current)
-    }
-
-    timeoutIdRef.current = setTimeout(() => {
-      if (
-        window.innerHeight + window.scrollY >= document.body.offsetHeight &&
-        !isAllLoaded
-      ) {
-        handleLoadMore()
-      }
-    }, 300)
-  }, [isAllLoaded, handleLoadMore])
+  const observerRef = useRef<IntersectionObserver | null>(null)
+  const targetRef = useRef<HTMLDivElement | null>(null)
 
   useEffect(() => {
-    window.addEventListener('scroll', handleScroll)
-    return () => {
-      window.removeEventListener('scroll', handleScroll)
-      if (timeoutIdRef.current) {
-        clearTimeout(timeoutIdRef.current)
+    const observerCallback = (entries: IntersectionObserverEntry[]) => {
+      const [entry] = entries
+      if (entry.isIntersecting && !isAllLoaded) {
+        handleLoadMore()
       }
     }
-  }, [handleScroll])
+
+    observerRef.current = new IntersectionObserver(observerCallback, {
+      root: null,
+      rootMargin: '0px',
+      threshold: 1.0,
+    })
+
+    const currentTarget = targetRef.current
+    if (currentTarget && observerRef.current) {
+      observerRef.current.observe(currentTarget)
+    }
+
+    return () => {
+      if (currentTarget && observerRef.current) {
+        observerRef.current.unobserve(currentTarget)
+      }
+    }
+  }, [handleLoadMore, isAllLoaded])
+
+  return targetRef
 }
