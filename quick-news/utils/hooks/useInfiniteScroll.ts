31f18cee7c73fef86a271b6fcf48a3206616ac79
Hchan2 <seongchan_@naver.com>
2024-09-14 21:31:19 +0900

    update: debounce 적용하여 추가 데이터 불러오기 대기시간 적용

--- quick-news/utils/hooks/useInfiniteScroll.ts
+++ quick-news/utils/hooks/useInfiniteScroll.ts
@@ -1,5 +1,6 @@
 import { useEffect, useRef } from 'react'
 import { InfiniteScrollProps } from '../types/type'
+import debounce from 'lodash/debounce'
 
 export default function useInfiniteScroll({
   handleLoadMore,
@@ -9,12 +10,12 @@ export default function useInfiniteScroll({
   const targetRef = useRef<HTMLDivElement | null>(null)
 
   useEffect(() => {
-    const observerCallback = (entries: IntersectionObserverEntry[]) => {
+    const observerCallback = debounce((entries: IntersectionObserverEntry[]) => {
       const [entry] = entries
       if (entry.isIntersecting && !isAllLoaded) {
         handleLoadMore()
       }
-    }
+    }, 500)
 
     observerRef.current = new IntersectionObserver(observerCallback, {
       root: null,
