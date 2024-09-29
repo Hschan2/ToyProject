c657affeeaf8ec47df6fe7dcc68b5c7f12555591
Hchan2 <seongchan_@naver.com>
2024-09-29 20:28:56 +0900

    update: Proxy를 활용한 Fetch SSR 모든 시도 실패로 CSR 교체 예정

--- quick-news/pages/news/NewsLists.tsx
+++ quick-news/pages/news/NewsLists.tsx
@@ -1,100 +1,45 @@
-import React, { useCallback, useEffect, useState } from 'react'
-import { GetServerSideProps } from 'next'
-import dynamic from 'next/dynamic'
+import React from 'react'
 import 'moment/locale/ko'
-import useMoreNews from '../../utils/hooks/useMoreNews'
-import NaverNewsFetch from '../fetch/NaverNewsFetch'
-import useInfiniteScroll from '../../utils/hooks/useInfiniteScroll'
-import { CommonNewsListProps, NaverNewsProps } from '../../utils/types/type'
-import Loading from '../loading/Loading'
-import Skeleton from '../loading/Skeleton'
-import axios from 'axios'
+import type { InferGetServerSidePropsType, GetServerSideProps } from 'next'
+import ClientNewsComponent from './ClientNewsComponent'
+import { NaverNewsProps } from '../../utils/types/type'
+import { MAX_PAGE_COUNT } from '../../utils/Constants'
 
-const NewsItem = dynamic(() => import('./NewsItem'), {
-  ssr: false,
-  loading: () => <Skeleton />,
-})
-const RenderNewsPage = dynamic(() => import('../page/render/RenderNewsPage'), {
-  ssr: false,
-  loading: () => <Loading />,
-}) as React.ComponentType<CommonNewsListProps<NaverNewsProps>>
-
-interface NewsDataProps {
-  initialNews: NaverNewsProps[]
-  initialPageSize: number
-  isAllLoaded: boolean
+interface NaverPropsRes {
+  items: NaverNewsProps[]
 }
 
-export default function NewsLists({
-  initialNews,
-  initialPageSize,
-  isAllLoaded: serverLoaded,
-}: NewsDataProps) {
-  const [news, setNews] = useState<NaverNewsProps[]>(initialNews || [])
-  const { pageSize, handleLoadMore, isAllLoaded } = useMoreNews(initialPageSize)
-  const { visibleNews, isLoading } = NaverNewsFetch(pageSize)
-  const targetRef = useInfiniteScroll({ handleLoadMore, isAllLoaded })
-  const [allLoaded, setAllLoaded] = useState(serverLoaded)
-
-  useEffect(() => {
-    if (visibleNews && visibleNews.length > 0) {
-      setNews((prevNews) => [...prevNews, ...visibleNews])
-    }
-    if (visibleNews && visibleNews.length < pageSize) {
-      setAllLoaded(true)
-    }
-  }, [visibleNews, pageSize])
-
-  const renderNewsItem = useCallback(
-    (article: NaverNewsProps) => (
-      <NewsItem key={article.id} article={article} />
-    ),
-    [],
-  )
-
-  return (
-    <>
-      <RenderNewsPage
-        visibleNews={news}
-        isLoading={isLoading}
-        itemRenderer={renderNewsItem}
-      />
-      {!allLoaded && <div ref={targetRef}></div>}
-    </>
-  )
-}
-
-export const getServerSideProps: GetServerSideProps<NewsDataProps> = async (
-  context,
-) => {
-  const pageSize = 10
+export const getServerSideProps = (async () => {
   const queryValue = '오늘의주요뉴스'
-  try {
-    const { data } = await axios.get('/api/naver-news-proxy', {
-      params: {
-        q: queryValue,
-        pageCount: pageSize,
-      },
-      headers: {
-        'X-Naver-Client-Id': process.env.NEXT_PUBLIC_CLIENT_ID,
-        'X-Naver-Client-Secret': process.env.NEXT_PUBLIC_CLIENT_SECRET,
-      },
-    })
-    return {
-      props: {
-        initialNews: data.items,
-        initialPageSize: pageSize,
-        isAllLoaded: data.items.length < pageSize,
-      },
-    }
-  } catch (error) {
-    console.error('뉴스 데이터 가져오기 실패', error)
-    return {
-      props: {
-        initialNews: [],
-        initialPageSize: pageSize,
-        isAllLoaded: true,
-      },
+  const url = `/api/naver-news-proxy?q=${queryValue}&pageCount=${MAX_PAGE_COUNT}`
+  const headers: HeadersInit = {
+    'X-Naver-Client-Id': process.env.NEXT_PUBLIC_CLIENT_ID!,
+    'X-Naver-Client-Secret': process.env.NEXT_PUBLIC_CLIENT_SECRET!,
+  }
+
+  const res = await fetch(url, {
+    headers,
+  })
+
+  if (!res.ok) {
+    if (res.status === 404) {
+      throw new Error('페이지를 찾을 수 없습니다.')
+    } else if (res.status >= 500) {
+      throw new Error('서버 오류가 발생했습니다.')
+    } else {
+      throw new Error(`뉴스 데이터를 가져오지 못했습니다. ${res.status}`)
     }
   }
+
+  const repo: NaverPropsRes = await res.json()
+  return {
+    props: { items: repo.items },
+    revalidate: 60,
+  }
+}) satisfies GetServerSideProps<{ items: NaverNewsProps[] }>
+
+export default function NewsLists({
+  items,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  return <ClientNewsComponent initialNews={items} initialPageSize={10} />
 }
