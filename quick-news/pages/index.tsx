e7331fce932118d694fd574cf67666ddf175e2fb
Hchan2 <seongchan_@naver.com>
2024-10-04 22:06:44 +0900

    update: 네이버 뉴스 데이터 SSR로 가져와 넘기기

--- quick-news/pages/index.tsx
+++ quick-news/pages/index.tsx
@@ -1,15 +1,54 @@
+import { NaverNewsProps } from '@/utils/types/type'
+import axios from 'axios'
 import { lazy } from 'react'
 
 const LazyNewsLists = lazy(() => import('./news/NewsLists'))
 const LazyContents = lazy(() => import('./page/render/Contents'))
 
-export default function Home() {
+interface NewsProps {
+  news: NaverNewsProps[]
+}
+
+export default function Home({ news }: NewsProps) {
   return (
     <LazyContents
       title="오늘의 주요뉴스"
       description="오늘의 주요뉴스를 확인하세요"
     >
-      <LazyNewsLists />
+      <LazyNewsLists newsData={news} />
     </LazyContents>
   )
+}
+
+export async function getServerSideProps() {
+  const query = '오늘의주요뉴스'
+  const url = 'https://openapi.naver.com/v1/search/news.json'
+
+  try {
+    const response = await axios.get(url, {
+      headers: {
+        'X-Naver-Client-Id': process.env.NEXT_PUBLIC_CLIENT_ID,
+        'X-Naver-Client-Secret': process.env.NEXT_PUBLIC_CLIENT_SECRET,
+      },
+      params: {
+        query,
+        display: 40,
+        start: 1,
+        sort: 'sim',
+      },
+    })
+
+    return {
+      props: {
+        news: response.data.items,
+      },
+    }
+  } catch (error) {
+    console.error('API 호출 에러: ', error)
+    return {
+      props: {
+        news: [],
+      },
+    }
+  }
 }
