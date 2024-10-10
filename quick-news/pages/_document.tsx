8d7faf8abf00a339bf5b3d9864cfd980d377007b
Hchan2 <seongchan_@naver.com>
2024-10-10 16:33:50 +0900

    update: font preload

--- quick-news/pages/_document.tsx
+++ quick-news/pages/_document.tsx
@@ -10,21 +10,28 @@ export default function Document() {
           sizes="16x16"
           rel="icon"
           href="/icons8-news-doodle-16.png"
-          as='icon-16'
+          as="icon-16"
         />
         <link
           type="image/png"
           sizes="32x32"
           rel="icon"
           href="/icons8-news-doodle-32.png"
-          as='icon-32'
+          as="icon-32"
         />
         <link
           type="image/png"
           sizes="96x96"
           rel="icon"
           href="/icons8-news-doodle-96.png"
-          as='icon-96'
+          as="icon-96"
+        />
+        <link
+          rel="preload"
+          href="/fonts/NewsCycle-Bold.woff"
+          as="font"
+          type="font/woff"
+          crossOrigin="anonymous"
         />
         <meta name="description" content="뉴스 빠르게 확인하기" />
       </Head>
