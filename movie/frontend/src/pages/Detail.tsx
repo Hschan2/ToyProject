15ac102883d364a7aae0d8f022d61327ffca850e
Hchan2 <seongchan_@naver.com>
2024-10-12 13:52:06 +0900

    update: memo 적용

--- movie/frontend/src/pages/Detail.tsx
+++ movie/frontend/src/pages/Detail.tsx
@@ -1,9 +1,18 @@
-import { useParams } from 'react-router-dom';
-import Loading from '../components/pages/loading/Loading';
-import SEO from '../components/pages/seo/SEO';
-import { ContainerUnderLine, DetailContainer, DetailImage, DetailInfoContainer, DetailOverview, DetailTitle, InfoSpan } from '../style/DetailPage';
-import { ErrorHandling } from '../hooks/ErrorHandling';
-import { useDetailQuery } from '../hooks/useMovieQuery';
+import { useParams } from "react-router-dom";
+import Loading from "../components/pages/loading/Loading";
+import SEO from "../components/pages/seo/SEO";
+import {
+  ContainerUnderLine,
+  DetailContainer,
+  DetailImage,
+  DetailInfoContainer,
+  DetailOverview,
+  DetailTitle,
+  InfoSpan,
+} from "../style/DetailPage";
+import { ErrorHandling } from "../hooks/ErrorHandling";
+import { useDetailQuery } from "../hooks/useMovieQuery";
+import { memo } from "react";
 
 /**
  * @param id // 영화 아이디
@@ -13,12 +22,12 @@ function Detail() {
   const { id } = useParams();
   const { status, data, error, isFetching } = useDetailQuery(id);
 
-  if (status === 'pending' || isFetching) {
+  if (status === "pending" || isFetching) {
     return <Loading />;
   }
 
   if (error) {
-    return <ErrorHandling error={error} viewName="Detail" />
+    return <ErrorHandling error={error} viewName="Detail" />;
   }
 
   return (
@@ -28,11 +37,21 @@ function Detail() {
         <Loading />
       ) : (
         <DetailContainer>
-          <DetailImage src={`https://image.tmdb.org/t/p/w500/${data.poster_path}`} alt={`${data.title}`} />
+          <DetailImage
+            src={`https://image.tmdb.org/t/p/w500/${data.poster_path}`}
+            alt={`${data.title}`}
+          />
           <DetailInfoContainer>
             <DetailTitle>{data.title || "로딩중..."}</DetailTitle>
-            <InfoSpan>프로덕션 | {data.production_companies.map(company => company.name).join(', ')}</InfoSpan>
-            <InfoSpan>장르 | {data.genres.map(genre => genre.name).join(', ')}</InfoSpan>
+            <InfoSpan>
+              프로덕션 |{" "}
+              {data.production_companies
+                .map((company) => company.name)
+                .join(", ")}
+            </InfoSpan>
+            <InfoSpan>
+              장르 | {data.genres.map((genre) => genre.name).join(", ")}
+            </InfoSpan>
             <InfoSpan>상영시간 | {data.runtime} 분</InfoSpan>
             <InfoSpan>평점 | {data.vote_average?.toFixed(1)}점</InfoSpan>
             <ContainerUnderLine />
@@ -41,7 +60,7 @@ function Detail() {
         </DetailContainer>
       )}
     </div>
-  )
+  );
 }
 
-export default Detail
+export default memo(Detail);
